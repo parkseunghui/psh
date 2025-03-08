2c9a9ba1660a665d62925864bd41b8658fca8f73
Parkseunghui <qkrtmdgmldla@gmail.com>
2025-03-08 19:24:13 +0900

    report

---
+++ AI week1(1).py
@@ -0,0 +1,76 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+print("Hello World")
+Hello World
+print('Hello', 'World')
+Hello World
+print('Hello'+'World')
+HelloWorld
+print(100)
+100
+print(2+4)
+6
+100
+100
+10+20
+30
+10-20
+-10
+10*20
+200
+10/20
+0.5
+123*8765
+1078095
+10+20+30
+60
+10+20*30
+610
+git branch -M main
+
+SyntaxError: invalid syntax
+git remote add origin https://github.com/parkseunghui/psh.git
+SyntaxError: invalid syntax
+git init
+SyntaxError: invalid syntax
+git init
+SyntaxError: invalid syntax
+>>> SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+>>> 
+>>> 
+>>> 
+>>> 
+>>> SyntaxError: invalid syntax
+SyntaxError: invalid syntax
+>>> 
+>>> x =100
+>>> y =200
+>>> x+y
+300
+>>> x-y
+-100
+>>> x*y
+20000
+>>> x/y
+0.5
+>>> x**2
+10000
+>>> n=5
+>>> n**2
+25
+>>> 25
+25
+>>> 
+>>> name =input('What is your name?: ')
+What is your name?: 
+>>> What is your name?:
+...     
+SyntaxError: invalid syntax
+>>> 
+>>> name = input('What is your name?: ')
+What is your name?: psh
+>>> 
+>>> name = 'psh'
+>>> print(name)
+psh
